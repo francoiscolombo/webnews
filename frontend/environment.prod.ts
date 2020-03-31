0df356b1b21b762f17cf6d12609e1e5e83ca777d
Francois Colombo <francois_colombo@yahoo.fr>
2020-03-31 16:40:33 +0200

    forgot the quotes :-P

--- frontend/environment.prod.ts
+++ frontend/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseURL: ${WEBNEWS_API_BASE_URL},
-  appName: ${FRONTEND_APP_NAME},
-  token: ${FRONTEND_APP_TOKEN},
-  categories: "Business,Entertainment,Health,Lifestyle,Politics,Technology,World",
+  baseURL: '${WEBNEWS_API_BASE_URL}',
+  appName: '${FRONTEND_APP_NAME}',
+  token: '${FRONTEND_APP_TOKEN}',
+  categories: 'Business,Entertainment,Health,Lifestyle,Politics,Technology,World',
   maxHeadlines: 10
 };
